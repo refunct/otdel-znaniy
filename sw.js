704244363c95ecf71b9cf26b4603c1652b1675d5
Refunct <dkondakov95@yandex.ru>
2026-04-22 15:22:44 +0500

    first commit7

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener("install", e=>{
   e.waitUntil(
-    caches.open("v1").then(c=>{
+    caches.open("v11").then(c=>{
       return c.addAll([
         "./",
         "./index.html",
